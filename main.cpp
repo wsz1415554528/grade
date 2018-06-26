4c3b9d9496047eadff6c695674fcc71549b139db
wsz1415554528 <s20161104602@mac22.local>
2018-06-26 11:52:53 +0800

    xiugai
    xiugaiu

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  grade
+//
+//  Created by s20161104602 on 18/6/25.
+//  Copyright © 2018年 s20161104602. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class result
+{
+private:
+    int id;
+    string name;
+    float score[5];
+    float ave;
+public:
+    result(int i,string n,float s[5])
+    {
+    float sum=0,max=s[]
+    
+    
+    
+    
+    }
+
+
+
+}
