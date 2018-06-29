c7c7ed0a4fc93024443c10ebcacd3a9e88a78203
wsz1415554528 <s20161104602@mac22.local>
2018-06-29 15:20:43 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -1,221 +1,132 @@
-//
-//  main.cpp
-//  grade
-//
-//  Created by s20161104602 on 18/6/25.
-//  Copyright © 2018年 s20161104602. All rights reserved.
-//
-
-
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<bits\stdc++.h>
+#define MAXSIZE 100
 using namespace std;
-const int UMPIRE=5;
-class Result{
-public:
-    Result(int n,char *p)//编号,姓名
-    {
-        if(strlen(p)<=0)
-            cout << " error" << endl;
-        num=n;
-        strcpy(name,p);
-        score=0;
-        memset(judgescore,0,UMPIRE);
-    }
-    Result()
-    {
-        num=0;
-        name[0]='/0';
-        score=0;
-        memset(judgescore,0,UMPIRE);
-    }
-    Result(const Result& rhs)
-    {
-        num=rhs.num;
-        strcpy(name,rhs.name);
-        score=rhs.score;
-        memcpy(judgescore,rhs.judgescore,UMPIRE);
-    }
-    void InitResult(int n,char *p)
-    {
-        if(strlen(p)<=0)
-            cout << " error" << endl;
-        num=n;
-        strcpy(name,p);
-        
-        score=0;
-        memset(judgescore,0,UMPIRE);
-    }
-    void SetJudgeScore(int *s)
-    {
-        int ix=0;
-        while(ix<UMPIRE)
-        {
-            judgescore[ix]=s[ix];
-            ix++;
-        }
-    }
-    
-    int* GetJudgeScore()
-    {
-        return judgescore;
-    }
-    int GetScore()
-    {
-        return score;
-    }
-    int GetNumber()
-    {
-        return num;
-    }
-    void ShowName()
-    {
-        cout << name;
-    }
-    int FindHighPos();
-    int FindLessPos();
-    int FindHigh()
-    {
-        return judgescore[FindHighPos()];
-    }
-    int FindLess()
-    {
-        return judgescore[FindLessPos()];
-    }
-    void DoCalculatorScore();
-    
-    friend ostream& operator<<(ostream &out,Result );
-    ~Result(){}
+
+class student{
 private:
-    int num; //参赛选手编号
-    char name[10];//姓名
-    int judgescore[UMPIRE];//裁判打分
-    int score;//得分
-    
-};
-ostream& operator<<(ostream &out,Result rhs)
-{
-    
-    out << rhs.GetNumber() << "/t";
-    rhs.ShowName();
-    out << "/t" << rhs.GetScore() << endl;
-    return out;
-}
-int Result::FindHighPos()
-{
-    int *pmax=max_element(judgescore,(judgescore+UMPIRE));
-    return pmax-judgescore;
-}
-int Result::FindLessPos()
-{
-    int *pmin=min_element(judgescore,(judgescore+UMPIRE));
-    return pmin-judgescore;
-}
-void Result::DoCalculatorScore()
-{
-    int maxpos=FindHighPos();
-    int minpos=FindLessPos();
-    cout << "maxpos:" <<maxpos << endl;
-    cout << "minpos:" <<minpos << endl;
-    for(int i=0; i<UMPIRE; i++)
-    {
-        if((i!=maxpos) && (i!=minpos))
-        {
-            score+=judgescore[i];
-        }
-    }
-    cout << "总成绩=" << score <<endl;
-    
-    if(FindHigh()==FindLess())
-        score = score/(UMPIRE);
-    else
-        score = score/(UMPIRE-2);
-    
-}
-bool lessbyscore( Result& l, Result& r)
-{
-    if(l.GetScore()<r.GetScore())
-        return true;
-    else
-        return false;
-}
-bool lessbynumber( Result& l, Result& r)
-{
-    if(l.GetNumber()<r.GetNumber())
-        return true;
-    else
-        return false;
-}
-int init(Result *&p);
-void SortByPlayerNumber();
-void SortByPlayerScore();
-int init(Result *&p)
-{
-    cout << "输入参赛选手人数:" << endl;
-    int n;
-    cin >> n;
-    cout << endl;
-    
-    if(!(p = new Result[n]))
-    {
-        cout << "error" << endl;
-        exit(1);
-    }
-    Result *pmv=p;
-    
-    int number;
-    char name[10];
-    for(int i=0; i<n; i++,pmv++)
-    {
-        cout << "输入所有参赛选手的编号及姓名:" << endl;
-        cin >> number >> name;
-        pmv->InitResult(number,name);
+    char name[20];
+    char sex;
+    char proName[20];
+    int proNum;
+    char className[20];
+    char phoneNum[20];
+public:
+    double socre[20];
+    double sumSocre;
+    student() {
         
-        cout << "开始对第" << i+1 <<"个参赛选手的成绩的打分"<< endl;
-        int tmpix=0;
-        int pjudgescore[UMPIRE];
-        while(tmpix<UMPIRE)
-        {
-            cout << "输入第" << (tmpix+1) << "个裁判的打分:";
-            cin >> pjudgescore[tmpix++];
-            
+    }
+    student getStuInformation(student d,int i) {
+        cout << endl << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ—ß…˙–’√˚£∫";
+        cin >> d.name;
+        cout << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ—ß…˙–‘±£¨mŒ™ƒ–£¨wŒ™≈Æ£∫";
+        cin >> d.sex;
+        cout << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ—ß…˙À˘≤Œº”µƒœÓƒø√˚≥∆∫Õ±‡∫≈£∫" << endl;
+        cout << "\t\tœÓƒø√˚≥∆£∫";
+        cin >> d.proName;
+        cout << "\t\tœÓƒø±‡∫≈£∫";
+        cin >> d.proNum;
+        cout << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ—ß…˙À˘‘⁄∞‡º∂£∫";
+        cin >> d.className;
+        cout << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ—ß…˙¡™œµ∑Ω Ω£∫";
+        cin >> d.phoneNum;
+        return d;
+    }
+    void showName(student d) {
+        cout << d.name;
+    }
+    void showSocre(student d,int i) {
+        cout << d.socre[i];
+    }
+    void operateSumSocre(student &d,int n) {
+        int sum;
+        for(int i = 0;i < n;i++) {
+            d.sumSocre += d.socre[i];
         }
-        cout << "结束对第" << n <<"个参赛选手的成绩的打分"<< endl;
-        pmv->SetJudgeScore(pjudgescore);
-        pmv->DoCalculatorScore();
-        cout << "去掉的最高分是:" << pmv->FindHigh() << "/t去掉最低分是:"
-        << pmv->FindLess() << "/t最后的得分是:" << pmv->GetScore() << endl;
-        tmpix=0;
     }
-    return n;
-}
-void SortByPlayerNumber(Result *p,int n)
-{
-    sort(p,p+n,lessbynumber);
-}
-void SortByPlayerScore (Result *p,int n)
-{
-    sort(p,p+n,lessbyscore);
-}
-int main()
-{
-    Result *r;
-    int n=init(r);
-    
-    cout << "按序号从低到高显示结果如下:" << endl;
-    SortByPlayerNumber(r,n);
-    for (int i=0; i<n; i++)
-    {
-        cout << r[i];
+    void sortPro(student d[],int n) {
+        student temp;
+        for(int i = 0;i < n;i++) {
+            for(int j = i;j < n;j++) {
+                if(d[i].proNum >= d[j].proNum) {
+                    temp = d[j];
+                    d[j] = d[i];
+                    d[i] = temp;
+                }
+            }
+        }
     }
-    cout <<endl;
-    cout << "按得分从低到高显示结果如下:" << endl;
-    SortByPlayerScore(r,n);
-    for (int i=0; i<n; i++)
-    {
-        cout << r[i];
+    void setStuInformation(student d) {
+        cout << "\t\t–’√˚£∫" << d.name << endl;
+        cout << "\t\t–‘±£∫" << d.sex << endl;
+        cout << "\t\t∞‡º∂£∫" << d.className << endl;
+        cout << "\t\t¡™œµ∑Ω Ω" << d.phoneNum << endl;
+        cout << "\t\tÀ˘≤Œº”œÓƒø±‡∫≈£∫" << d.proNum << endl;
+        cout << "\t\tÀ˘≤Œº”œÓƒø√˚≥∆£∫" << d.proName << endl;
+        cout << "\t\tÀ˘µ√◊‹∑÷Œ™£∫" << d.sumSocre << endl;
     }
-    
+};
+
+class referee{
+private:
+    char name[20];
+    char sex;
+    char phoneNum[20];
+public:
+    referee() {
+        
+    }
+    referee getRefInformation(referee d,int i) {
+        cout << endl << "\t\t«Î ‰»Îµ⁄" << i << "∏ˆ≤√≈––’√˚";
+        cin >> d.name;
+        cout << "\t\t«Î ‰»Î≤√≈––‘±£∫";
+        cin >> d.sex;
+        cout << "\t\t«Î ‰»Î≤√≈–¡™œµ∑Ω Ω£∫";
+        cin >> d.phoneNum;
+        return d;
+    }
+    void showName(referee d) {
+        cout << d.name;
+    }
+    void setSocre(student &d,referee s,int i) {
+        cout << endl << "\tœ÷‘⁄ «\t" ;
+        s.showName(s);
+        cout << "\t≤√≈–∂‘\t" ;
+        d.showName(d);
+        cout << "\t—ß…˙Ω¯––∆¿∑÷\t" << endl;
+        cout << "\t«Î ‰»Î∏√—ß…˙µƒµ√∑÷£∫";
+        cin >> d.socre[i];
+    }
+};
+
+int main() {
+    int n;
+    int m;
+    student d[MAXSIZE];
+    referee s[MAXSIZE];
+    cout << "\t\t«Î ‰»Î≤Œ»¸—ß…˙ ˝£∫";
+    cin >> n;
+    cout << "\t\t«Î ‰»Î≤√≈– ˝£∫";
+    cin >> m;
+    for(int i = 0;i < n;i++) {
+        d[i] = d[i].getStuInformation(d[i],i+1);
+    }
+    for(int i = 0;i < m;i++) {
+        s[i] = s[i].getRefInformation(s[i],i+1);
+    }
+    for(int i = 0 ;i < n;i++) {
+        for(int j = 0; j < m;j++) {
+            s[j].setSocre(d[i],s[j],j);
+        }
+    }
+    d[0].sortPro(d,n);
+    for(int i = 0;i < n;i++) {
+        d[i].operateSumSocre(d[i],m);
+    }
+    for(int i = 0;i < n;i++) {
+        cout << endl << "\tµ⁄" << i+1 << "∏ˆ—ß…˙" << endl;
+        d[i].setStuInformation(d[i]); 
+        cout << endl;
+    }
     return 0;
-}
+} 
